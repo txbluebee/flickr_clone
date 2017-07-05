ae774a35823a9bdeef172acd9827828f54715d87
Anna Horodetska and Brian Fan <anna+brian@>
2017-07-05 15:31:13 -0700

    add edit delete function to image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -6,6 +6,7 @@ class ImagesController < ApplicationController
   def show
     @image = Image.find(params[:id])
     @image_owner = User.find(@image.user_id)
+    @current_user = current_user
   end
 
   def new
@@ -23,7 +24,27 @@ class ImagesController < ApplicationController
     end
   end
 
+  def edit
+    @image = Image.find(params[:id])
+    @user = current_user
+  end
 
+  def update
+    @image = Image.find(params[:id])
+    @user = current_user
+    if @image.update(image_params)
+      redirect_to image_path(@image), notice: "Image successfully updated!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @image = Image.find(params[:id])
+
+    @image.destroy
+    redirect_to images_path
+  end
 
   private
 
